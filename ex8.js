4a8f3e441e330b46e08849595dde5cfabad52e2a
Rauane Lima <rauanelima7@gmail.com>
2022-10-24 10:58:54 -0300

    ex de OO

--- ex8.js
+++ ex8.js
@@ -12,8 +12,24 @@ class pessoas{
   calcularImc(){
     return this.peso / (this.altura * this.altura)
   }
-
+  classificarImc(){
+    const imc = this.calcularImc();
+    if (imc < 18.5) {
+      return "Abaixo do peso"
+    } else if (imc >= 18.5 && imc <= 25) {
+      return "peso normal"
+    } else if (imc >= 25 && imc <= 30) {
+      return "Acima do peso"
+    } else if (imc >= 30 && imc <= 40) {
+      return "Obeso"
+    } else {
+      return "Obesidade grave"
+    }
+  }
 }
 
-const josé = new pessoas("José", 70, 1.75)
-console.log(josé.calcularImc())
+const jose = new pessoas("José", 70, 1.75)
+console.log(jose.classificarImc())
+
+const rauane = new pessoas("Rauane", 48, 1.65)
+console.log(rauane.classificarImc())
